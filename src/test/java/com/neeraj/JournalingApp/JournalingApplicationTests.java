07af35c52e26c5c52729857488056a19d22acf54
Neeraj Malik <neerajmalikak47@gmail.com>
2026-06-29 11:28:42 +0530

    Initial Commit - Created REST APIs

---
+++ src/test/java/com/neeraj/JournalingApp/JournalingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.JournalingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
